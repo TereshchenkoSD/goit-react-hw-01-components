e08e7c3b0f095824c793e96e83b98cf972de73d8
TereshchenkoSD <serghh1988@gmail.com>
2021-06-25 17:01:09 +0300

    Установил все зависимостию. Создал файлы json с данными для каждого компонента. Прописал рендер компонента Profile.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-// import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import profile from './profile.json';
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
+function Profile(props) {
+  console.log(props);
+  return (
+    <div class="profile">
+      <div class="description">
+        <img src={props.url} alt={props.profileName} class="avatar" />
+        <p class="name">{props.profileName}</p>
+        <p class="tag">{props.tagName}</p>
+        <p class="location">{props.location}</p>
+      </div>
 
-// // If you want to start measuring performance in your app, pass a function
-// // to log results (for example: reportWebVitals(console.log))
-// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
+      <ul class="stats">
+        <li>
+          <span class="label">Followers</span>
+          <span class="quantity">{props.followerQuantity}</span>
+        </li>
+        <li>
+          <span class="label">Views</span>
+          <span class="quantity">{props.viewsQuantity}</span>
+        </li>
+        <li>
+          <span class="label">Likes</span>
+          <span class="quantity">{props.likesQuantity}</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+const data = profile;
+
+ReactDOM.render(
+  <Profile
+    url={data.avatar}
+    profileName={data.name}
+    tagName={data.tag}
+    location={data.location}
+    followerQuantity={data.stats.followers}
+    viewsQuantity={data.stats.views}
+    likesQuantity={data.stats.lprops}
+  />,
+  document.querySelector('#root'),
+);
